d16b4ebac4c1947a8b178c6b8991022ed5aed784
shrikant <shrikantj0794@gmail.com>
2024-09-12 16:24:02 +0530

    m

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,11 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const connectDB = require('./config/db');
 const router = require('./routes'); // Correctly importing the router from Routes
-
+const path = require('path')//1
 const app = express();
+const _dirname = path.dirname('')//2
+const buildpath = path.join(_dirname, "../frontend/build")//3
+app.use(express.static(buildpath))//4
 
 app.use(cors({
     origin: 'http://localhost:3000',
